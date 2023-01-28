6243f381d70296d94c070c56bff6b123e1e2d109
GabrielRodrigues07 <grodrigues256@gmail.com>
2023-01-28 08:03:36 -0300

    projeto inicial

---
+++ src/main/java/br/com/restgateway/RestGatewayApplication.java
@@ -0,0 +1,13 @@
+package br.com.restgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/restgateway/RestGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.restgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
